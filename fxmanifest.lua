8d46be9b16ee9006f1f4562ce6a3aa8ac87f50ec
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-10 19:16:23 +0100

    version check

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,5 @@
 fx_version "adamant"
-
 games { 'rdr3'}
-
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 
@@ -18,4 +16,9 @@ server_scripts {
 	'config.lua',
     'server/server.lua',
 }
+
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/VORPCORE/vorp-barbershop_lua'
 
