f2552b50c5b4593cf706edd767fa8422590e24e5
Honor <honormik@gmail.com>
2022-05-27 21:20:24 +0200

    Add italian language and locale language system

---
+++ languages/en_lang.lua
@@ -0,0 +1,16 @@
+--------------------------------------------------------------------------------------
+---------------------------------- English -------------------------------------------
+
+
+Locales["en_lang"] = {
+    openmenu = "Open Menu",
+	barbershop = "Barbershop",
+	hair = "Hair",
+	hair2 = "Style Your Hair",
+	beard = "Beard",
+	beard2 = "Style Your Beard",
+	confirm = "Confirm Selection",
+	confirm2 = "Price $",
+	youpaid = "Paid $",
+	nomoney = "Not Enough Cash",
+}
